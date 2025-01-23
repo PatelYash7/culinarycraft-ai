3f80f5498644d470eec2a615b7e2407246a167dc
Yash Patel <109963122+PatelYash7@users.noreply.github.com>
2025-01-23 16:28:50 +0530

    Added ui libs.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+'use client'
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-8 row-start-2 items-center  sm:items-start">
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -12,6 +14,9 @@ export default function Home() {
           height={38}
           priority
         />
+        <Button>
+          Hello
+        </Button>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
